209af776ee67fc7c7572e0146713944a44c5b3ec
secona <77039267+secona@users.noreply.github.com>
2024-05-14 13:52:07 +0700

    feat: update user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,57 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, InternalServerErrorException, Param, Patch, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
   constructor(
     private usersService: UsersService,
-  ) {};
+  ) { };
 
   @Get()
   async getUsers(): Promise<User[]> {
     return this.usersService.getUsers();
   }
 
+  @Get(":userId")
+  async getUser(@Param("userId") userId: string): Promise<User> {
+    return this.usersService.getUser(userId);
+  }
+
   @Post()
   async createUser(@Body() data: Record<string, any>): Promise<User> {
-    return this.usersService.createUser({
+    const user = await this.usersService.createUser({
       name: data.name,
       username: data.username,
       password: data.password,
-    })
+    }).catch(e => {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === "P2002") {
+          throw new BadRequestException("User with the same username already exists.");
+        }
+      }
+
+      throw new InternalServerErrorException();
+    });
+
+    return user;
   }
+
+  @Patch(":userId")
+  async updateUser(
+    @Param("userId") userId: string,
+    @Body() data: Record<string, any>
+  ): Promise<User> {
+    const user = await this.usersService.updateUser(userId, {
+      name: data.name,
+    });
+
+    return user;
+  }
+
+  // TODO
+  // @Delete(":userId")
+  // async deleteUser(@Param("userId") userId: string) {
+  //   this.usersService.deleteUser(userId);
+  // }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,15 +4,30 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {};
+  constructor(private prisma: PrismaService) { };
 
   async getUsers(): Promise<User[]> {
     return this.prisma.user.findMany();
   }
 
+  async getUser(id: string): Promise<User> {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({
+    return this.prisma.user.create({ data });
+  }
+
+  async updateUser(id: string, data: Prisma.UserUpdateInput): Promise<User> {
+    return this.prisma.user.update({
+      where: { id },
       data,
+    })
+  }
+
+  async deleteUser(id: string) {
+    this.prisma.user.delete({
+      where: { id },
     });
   }
 }
